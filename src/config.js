e199823026113aeebc5119a42ae09eba785a1be0
kumarprashant24 <kumar.prashant@tothenew.com>
2022-12-20 12:31:32 +0530

    changed backend url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export let API_URL;
 
 if (process.env.REACT_APP_STAGE === 'prod') {
-  API_URL = 'https://buzz-backend-x37y.onrender.com/api';
+  API_URL = 'https://buzz-app-backend.onrender.com/api';
 }
 if (process.env.REACT_APP_STAGE === 'dev') {
   API_URL = 'http://localhost:5000/api';
